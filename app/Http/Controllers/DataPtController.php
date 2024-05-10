d27319242c28f90106b034bf774415e0af1c4677
Koutsura <m.denny.trilisandi@gmail.com>
2024-05-10 22:08:03 +0700

    data_pt,data_mhs,data_prodi controller dan models

---
+++ app/Http/Controllers/DataPtController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\data_pt;
+use Illuminate\Http\Request;
+
+class DataPtController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(data_pt $data_pt)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(data_pt $data_pt)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, data_pt $data_pt)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(data_pt $data_pt)
+    {
+        //
+    }
+}
