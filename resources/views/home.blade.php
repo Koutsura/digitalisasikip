d141e78b3366cd3a235d1165deadeba265558f97
azizah <jiotunch@gmail.com>
2024-05-20 10:52:39 +0700

    add search bar dan menambah batch

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,10 +10,20 @@
 <div class="main-content">
     <section class="section">
         <div class="section-header">
-            <h1>Dashboard</h1>
+            <h1>Batch Mahasiswa</h1>
         </div>
 
         <div class="section-body">
+            <div class="row mb-4">
+                <div class="col">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Cari Batch...">
+                        <div class="input-group-append">
+                            <button class="btn btn-primary" type="button">Cari</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="row">
                 <div class="col-md-4">
                     <div class="card">
@@ -38,6 +48,33 @@
                         <div class="card-body">
                             <h5 class="card-title">Batch 3</h5>
                             <p class="card-text">Deskripsi atau informasi tambahan tentang Batch 3.</p>
+                            <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Batch 4</h5>
+                            <p class="card-text">Deskripsi atau informasi tambahan tentang Batch 4.</p>
+                            <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Batch 5</h5>
+                            <p class="card-text">Deskripsi atau informasi tambahan tentang Batch 5.</p>
+                            <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Batch 6</h5>
+                            <p class="card-text">Deskripsi atau informasi tambahan tentang Batch 6.</p>
                             <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
                         </div>
                     </div>
