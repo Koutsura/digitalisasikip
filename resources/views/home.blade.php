130c84ccb1c10ac058c9a63fc59168bfa1d726e0
azizah <jiotunch@gmail.com>
2024-05-20 10:53:47 +0700

    change title dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
 <div class="main-content">
     <section class="section">
         <div class="section-header">
-            <h1>Batch Mahasiswa</h1>
+            <h1>Dashboard</h1>
         </div>
 
         <div class="section-body">
