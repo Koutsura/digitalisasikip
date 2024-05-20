691981d5f2dda5ce838b7fc3183f918f40ab42a1
azizah <jiotunch@gmail.com>
2024-05-20 10:34:27 +0700

    ubah tampilan dashboard dengan batch

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,55 @@
 @extends('layouts.app')
 
+@section('title', 'Batch Mahasiswa')
+
+@push('style')
+    <!-- CSS Libraries -->
+@endpush
+
 @section('content')
 <div class="main-content">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <section class="section">
+        <div class="section-header">
+            <h1>Dashboard</h1>
+        </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+        <div class="section-body">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Batch 1</h5>
+                            <p class="card-text">Deskripsi atau informasi tambahan tentang Batch 1.</p>
+                            <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Batch 2</h5>
+                            <p class="card-text">Deskripsi atau informasi tambahan tentang Batch 2.</p>
+                            <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Batch 3</h5>
+                            <p class="card-text">Deskripsi atau informasi tambahan tentang Batch 3.</p>
+                            <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 </div>
 @endsection
+
+@push('scripts')
+    <!-- JS Libraries -->
+
+    <!-- Page Specific JS File -->
+@endpush
