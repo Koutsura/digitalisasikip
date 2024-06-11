ba8a849867c1be0de261076f43a6dd29fecd1cb5
Koutsura <m.denny.trilisandi@gmail.com>
2024-06-11 12:29:30 +0700

    import

--- app/Imports/dataMhsImport.php
+++ app/Imports/dataMhsImport.php
@@ -10,6 +10,7 @@ use Maatwebsite\Excel\Concerns\ToModel;
 
 class dataMhsImport implements ToCollection, ToModel
 {
+    private $current =0;
     /**
     * @param Collection $collection
     */
@@ -20,44 +21,49 @@ class dataMhsImport implements ToCollection, ToModel
     }
     public function model(array $row)
     {
-        return new peserta([
-            'nik' => $row[0],
-            'no_pendaftaran' => $row[1],
-            'nisn' => $row[2],
-            'npsn' => $row[3],
-            'email' => $row[4],
-            'nim' => $row[5],
-            'no_kk' => $row[6],
-            'nama_mahasiswa' => $row[7],
-            'tempat_lahir' => $row[8],
-            'tanggal_lahir' => $row[9],
-            'jenis_kelamin' => $row[10],
-            'alamat' => $row[11],
-            'no_hp' => $row[12],
-            'no_kip' => $row[13],
-            'no_kks' => $row[14],
-            'asal_sekolah' => $row[15],
-            'kab_kota_sekolah' => $row[16],
-            'prov_sekolah' => $row[17],
-            'nama_ayah' => $row[18],
-            'pekerjaan_ayah' => $row[19],
-            'penghasilan_ayah' => $row[20],
-            'status_ayah' => $row[21],
-            'nama_ibu' => $row[22],
-            'pekerjaan_ibu' => $row[23],
-            'penghasilan_ibu' => $row[24],
-            'status_ibu' => $row[25],
-            'jumlah_tanggungan' => $row[26],
-            'kepemilikan_rumah' => $row[27],
-            'tahun_perolehan_rumah' => $row[28],
-            'sumber_listrik' => $row[29],
-            'luas_tanah' => $row[30],
-            'luas_bangunan' => $row[31],
-            'sumber_air' => $row[32],
-            'mck' => $row[33],
-            'jarak_pusat_kota_km' => $row[34],
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        $this->current++;
+        if($this->current > 1)
+        {
+            $peserta = new peserta;
+$peserta->nik = $row[0];
+$peserta->no_pendaftaran = $row[1];
+$peserta->nisn = $row[2];
+$peserta->npsn = $row[3];
+$peserta->email = $row[4];
+$peserta->nim = $row[5];
+$peserta->no_kk = $row[6];
+$peserta->nama_mahasiswa = $row[7];
+$peserta->tempat_lahir = $row[8];
+$peserta->tanggal_lahir = $row[9];
+$peserta->jenis_kelamin = $row[10];
+$peserta->alamat = $row[11];
+$peserta->no_hp = $row[12];
+$peserta->no_kip = $row[13];
+$peserta->no_kks = $row[14];
+$peserta->asal_sekolah = $row[15];
+$peserta->kab_kota_sekolah = $row[16];
+$peserta->prov_sekolah = $row[17];
+$peserta->nama_ayah = $row[18];
+$peserta->pekerjaan_ayah = $row[19];
+$peserta->penghasilan_ayah = $row[20];
+$peserta->status_ayah = $row[21];
+$peserta->nama_ibu = $row[22];
+$peserta->pekerjaan_ibu = $row[23];
+$peserta->penghasilan_ibu = $row[24];
+$peserta->status_ibu = $row[25];
+$peserta->jumlah_tanggungan = $row[26];
+$peserta->kepemilikan_rumah = $row[27];
+$peserta->tahun_perolehan_rumah = $row[28];
+$peserta->sumber_listrik = $row[29];
+$peserta->luas_tanah = $row[30];
+$peserta->luas_bangunan = $row[31];
+$peserta->sumber_air = $row[32];
+$peserta->mck = $row[33];
+$peserta->jarak_pusat_kota_km = $row[34];
+$peserta->save();
+
+
+
+    }
     }
 }
