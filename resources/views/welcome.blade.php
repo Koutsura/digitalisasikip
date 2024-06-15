781b184029dd250fedcbd6e815fc5a69823e2f04
Koutsura <m.denny.trilisandi@gmail.com>
2024-06-15 19:31:18 +0700

    tampilan mobile welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -130,39 +130,35 @@
         }
         .sistem-pencairan-kip-kuliah-merdeka {
             font-size: 24px;
-            left: 20px;
-            top: 100px;
+            margin: 20px auto;
             width: auto;
             text-align: center;
         }
         .kartu-indonesia-pintar-kuliah-kip-kuliah-adalah-salah-satu-upaya-untuk-membantu-asa-para-siswa-yang-memiliki-keterbatasan-ekonomi-tetapi-berprestasi-untuk-melanjutkan-studi-di-perguruan-tinggi {
             font-size: 16px;
-            left: 20px;
-            top: 200px;
+            margin: 20px 20px;
             width: auto;
-            margin-right: 20px;
             text-align: center;
         }
         ._1-2 {
             width: 100%;
             height: auto;
-            left: 0;
-            top: 300px;
+            margin-top: 20px;
         }
         .button, .button2 {
             width: 100%;
             max-width: 200px;
             margin: 10px auto;
-            position: static;
             display: block;
         }
         .whats-app-image-2024-03-06-at-14-23, .logo-kampus-merdeka-kemendikbud-1 {
             width: 100px;
             height: auto;
             margin: 20px auto;
-            position: static;
             display: block;
         }
+    
+
     }
 </style>
 
